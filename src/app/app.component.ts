88badf3b4dcca9e825901dda4c413a37de339ef9
royer <i2226824@continental.edu.pe>
2023-10-20 19:27:25 -0500

    mi primera app con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { DataService } from "./data.service";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+
+  name: string = 'rodrigo quesada';
+  age: number = 28;
+
+  users: string[] = ['juan', 'beto', 'carlos'];
+
+  posts: any[] = [];
+
+  constructor(private dataService: DataService){
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+  }
+
+  addUser(newUser:any) {
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+
+  deleteUser(user:any) {
+    for (let i = 0; i < this.users.length; i++)
+    if (user == this.users[i]) {
+      this.users.splice(i, 1);
+    }
+  }
+
 }
